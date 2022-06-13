581862d594f75815c98a11d1ecac016dc30a99b0
Minoru Nakano <minoru.nakano@gmail.com>
2022-06-13 14:37:33 -0400

    update get_MultiAssayExp

--- code/get_MultiAssayExp.R
+++ code/get_MultiAssayExp.R
@@ -2,13 +2,12 @@ library(Biobase)
 library(SummarizedExperiment)
 library(MultiAssayExperiment)
 library(stringr)
-
-source(paste(source_location, "code", "CNA_Functions.R", sep="/"))
-source(paste(source_location, "code", "SNV_Functions.R", sep="/"))
-source(paste(source_location, "code", "Create_ExpressionSets.R", sep="/"))
        
 get_MultiAssayExp <- function(study, input_dir, source_location){
-
+  source(paste(source_location, "code", "CNA_Functions.R", sep="/"))
+  source(paste(source_location, "code", "SNV_Functions.R", sep="/"))
+  source(paste(source_location, "code", "Create_SummarizedExp.R", sep="/"))
+  
   data = read.csv( paste(source_location, "data", "DATASET_LOAD_INFO.csv", sep="/") , sep=";" , stringsAsFactors=FALSE )
   data <- data[data$study == study, ]
   
