a66a4c1ad9d88dd78d196040de87d5037e192378
Minoru Nakano <minoru.nakano@gmail.com>
2022-11-25 19:01:24 -0500

    added clin and cased_sequenced file formatting script

---
+++ code/format_clin_cased_sequenced.R
@@ -0,0 +1,14 @@
+library(data.table)
+
+args <- commandArgs(trailingOnly = TRUE)
+work_dir <- args[1]
+
+case <- read.csv( file.path(work_dir, 'cased_sequenced.csv') , sep=";" , stringsAsFactors=FALSE )
+case$patient <- str_replace_all(case$patient, '[-\\.]', '_')
+clin <- read.csv( file.path(work_dir, 'CLIN.csv') , sep=";" , stringsAsFactors=FALSE )
+clin$patient <- str_replace_all(clin$patient, '[-\\.]', '_')
+clin$tissueid[is.na(clin$tissueid)] <- ""
+clin$treatmentid[is.na(clin$treatmentid)] <- ""
+
+saveRDS(case, file.path(work_dir, 'cased_sequences.rds'))
+saveRDS(case, file.path(work_dir, 'CLIN.rds'))
